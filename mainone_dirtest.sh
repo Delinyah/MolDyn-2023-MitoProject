ae8354993a45705a189484b1647c2c977e68d3fb
Delinyah <99490065+Delinyah@users.noreply.github.com>
2023-03-09 15:36:23 +0100

    Update mainone_dirtest.sh

--- mainone_dirtest.sh
+++ mainone_dirtest.sh
@@ -5,7 +5,7 @@ function help {
   echo "" 
   echo -e "\033[38;5;208mAutomated Workflow for Outer Mitochondrial Membrane Simulation Modeling\033[0m"   
   echo -e "\033[38;5;226mTitle: mainone (for MArtinize-INsanify Outer-membraNE...\033[0m"  
-  echo -e "\033[38;5;34m"Author: Delinyah C. Koning (as of March 9, 2023)\033[0m"
+  echo -e "\033[38;5;34mAuthor: Delinyah C. Koning (as of March 9, 2023)\033[0m"
   echo ""
   echo "Usage: omain.sh <pdb_code> [-nt <number_of_threads>]"
   echo "Don't forget to give permission to execute this file (chmod +x OMMautomate.sh)."
