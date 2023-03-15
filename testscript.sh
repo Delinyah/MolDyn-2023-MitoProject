fbcb3d1652bad3b6282d445f0cd0286e3fd18e8c
Delinyah <99490065+Delinyah@users.noreply.github.com>
2023-03-15 14:44:26 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -86,12 +86,12 @@ fi
 echo -e "\033[38;5;226mCoarse graining your system...\033[0m"
 ~/Project/OMM/martinize.py -f ${pdb_code}.pdb -o topol.top -x ${cg_pdb} -dssp /usr/bin/dssp -p backbone -ff martini22
 
-# Building initial configuration
+# Building initial configuration + writing text file with stderr output
 echo -e "\033[38;5;226mHOld on, building system...\033[0m"
-~/Project/OMM/insane -u POPC:5.5 -u CHOL:0.5 -u SAPE:4 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -l POPC:5.5 -l CHOL:0.5 -l PAPI:2 -l SAPE:2 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -d 10 -o system.gro -p topol.top -f ${cg_pdb} -center -pbc hex -sol W -salt 0 -excl -1
+~/Project/OMM/insane -u POPC:5.5 -u CHOL:0.5 -u SAPE:4 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -l POPC:5.5 -l CHOL:0.5 -l PAPI:2 -l SAPE:2 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -d 10 -o system.gro -p topol.top -f ${cg_pdb} -center -pbc hex -sol W -salt 0 -excl -1 2>>insane_stderr.txt
 
 # Modify topol.top include statements based on Insane output
-xxxx
+cat insane_stderr.txt >> topol.top
 
 # Add other needed include topology statements to topol.top
 sed -i 's/#include "martini.itp"/#include "..\/martini_v2.2.itp"\n#include "..\/SAPE.itp"\n#include "..\/martini_v2.0_ions.itp"\n#include "..\/martini_v2.0_lipids_all_201506.itp"/; s/\bProtein\b/Protein/g' topol.top
