9157cf805495cd3b56490db4c3bf3fc89dce4603
Delinyah <99490065+Delinyah@users.noreply.github.com>
2023-03-15 14:30:50 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -94,6 +94,9 @@ echo -e "\033[38;5;226mHOld on, building system...\033[0m"
 echo ';' >> topol.top
 ~/Project/OMM/insane -u POPC:5.5 -u CHOL:0.5 -u SAPE:4 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -l POPC:5.5 -l CHOL:0.5 -l PAPI:2 -l SAPE:2 -alname SAPE -alhead 'E P' -allink 'G G' -altail 'DDDDC CCCC' -d 10 -o system.gro -p topol.top -f ${cg_pdb} -center -pbc hex -sol W -salt 0 -excl -1 2>&1 | tee -a topol.top
 
+# Add needed include topology statements to topol.top
+sed -i 's/#include "martini.itp"/#include "..\/martini_v2.2.itp"\n#include "..\/SAPE.itp"\n#include "..\/martini_v2.0_ions.itp"\n#include "..\/martini_v2.0_lipids_all_201506.itp"/; s/\bProtein\b/Protein/g' topol.top
+
 # EM
 echo -e "\033[38;5;226mEnergy minimizing...\033[0m"
 echo "${min_mdp}" > minimization.mdp
