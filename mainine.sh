95087c527d77339588cb844b683c68d1fa7a680b
Delinyah <99490065+Delinyah@users.noreply.github.com>
2023-03-17 08:57:52 +0100

    Update mainine.sh

--- mainine.sh
+++ mainine.sh
@@ -17,24 +17,31 @@ function help {
   echo "1000-step EM"
   echo "1500ps NVT (dt 0.03, nstxout 20, v-rescale coupling system)"
   echo "1500ps NPT (dt 0.03, nstxout 20, parrinello-rahman pressure coupling, semiisotropic)"
-  echo "30ns production run (dt 0.03, nstep 1000000, nstxout 200)" 
+  echo "by default a 100ns production run (dt 0.03, nstep 3333333, nstxout 1000 >>> 3333 frames total, 30ps per frame)" 
   echo ""
   echo "Notes:"  
   echo "The pdb-code must be specified without .pdb extension (just the 4-letter code)."
-  echo "The pdb-file is the only file that should be present in the working directory. Fetching structures from PDB or OPM will be available in the future."
-  echo "After running, all files in the working directory will be moved (not copied) to a folder that is named after the pdb-code."
-  echo "Moving files will be done by moving all (*) files with a certain extension type. The .pdb files are specified manually to prevent mixup."
+  echo "The pdb-file is the only file that should be present in the working directory (+ this .sh script). Fetching structures from PDB or OPM will be available in the future."
+  echo "During running, the pdb-input in the initial working directory will be moved (not copied) to a folder that is named after the pdb-code."
+  echo "During the script, the new directory will become the new working directory; this makes sure that all output is collected."
+  echo "The topol.top file after coarse-graining is adapted by appending it with the stderr output after execution of the insane command. Do NOT change."
   echo ""
   echo "Flag options:"
   echo "  -h, --help: Show this help message and exit."
   echo "  -nt: Number of threads to use in GROMACS simulations (default is 1)."
   echo ""
   echo "Prerequisites:"
-  echo "  - .mdp files are located in the home directory (they will be sourced and copied to the working directory)."
+  echo "  - .mdp files are located in the home directory (specify path as needed)."
   echo "  - The PDB files to be used MUST be downloaded in the working directory."
-  echo "  - The DSSP library is located in /usr/bin/dssp. If not, adapt script."
+  echo "  - The DSSP library is assumed to be located in /usr/bin/dssp. If not, adapt script."
   echo "  - Insane executable, Martinate.py, and all required .itp files are in the home directory. They will not be copied to the working directory."
   echo ""
+}
+
+# Error function
+function error_exit {
+  echo -e "\033[38;5;196mMwah-Mwah..... Something is wrong here...\033[0m" >&2
+  exit 1
 }
 
 # Parse command line arguments
@@ -84,7 +91,7 @@ fi
 
 # Coarse-graining
 echo -e "\033[38;5;226mCoarse graining your system...\033[0m"
-~/Project/OMM/martinize.py -f ${pdb_code}.pdb -o topol.top -x ${cg_pdb} -dssp /usr/bin/dssp -p backbone -ff martini22
+~/Project/OMM/martinize.py -f ${pdb_code}.pdb -o topol.top -x ${cg_pdb} -dssp /usr/bin/dssp -p backbone -ff martini22 || error_exit
 
 # Make sure that when the following command edits topol.top no new stuff is added to existing lines
 echo ';' >> topol.top
@@ -99,43 +106,32 @@ sed -i 's/#include "martini.itp"/#include "..\/martini_v2.2.itp"\n#include "..\/
 # EM
 echo -e "\033[38;5;226mEnergy minimizing...\033[0m"
 echo "${min_mdp}" > minimization.mdp
-gmx grompp -p topol.top -f ~/Project/OMM/minimization.mdp -c system.gro -o minimization.tpr -maxwarn 1
-gmx mdrun -v -deffnm em -s minimization.tpr -nt $nt
+gmx grompp -p topol.top -f ~/Project/OMM/minimization.mdp -c system.gro -o minimization.tpr -maxwarn 1 || error_exit
+gmx mdrun -v -deffnm em -s minimization.tpr -nt $nt || error_exit
 
 # NVT
 echo -e "\033[38;5;226mNVT equilibration...\033[0m"
 echo "${nvt_mdp}" > nvt.mdp
-gmx grompp -f ~/Project/OMM/nvt.mdp -c em.gro -p topol.top -o nvt.tpr -maxwarn 1
-gmx mdrun -v -deffnm nvt -s nvt.tpr -nt $nt
+gmx grompp -f ~/Project/OMM/nvt.mdp -c em.gro -p topol.top -o nvt.tpr -maxwarn 1 || error_exit
+gmx mdrun -v -deffnm nvt -s nvt.tpr -nt $nt || error_exit
 
 # NPT
 echo -e "\033[38;5;226mNPT equilibration...\033[0m"
 echo "${npt_mdp}" > npt.mdp
-gmx grompp -f ~/Project/OMM/npt.mdp -c nvt.gro -p topol.top -o npt.tpr -maxwarn 1
-gmx mdrun -v -deffnm npt -s npt.tpr -nt $nt
+gmx grompp -f ~/Project/OMM/npt.mdp -c nvt.gro -p topol.top -o npt.tpr -maxwarn 1 || error_exit
+gmx mdrun -v -deffnm npt -s npt.tpr -nt $nt || error_exit
 
 # Production run
-echo -e "\033[38;5;226mCalculating RMSD, RMSF, and Rg after run...\033[0m"
-yes 0 | head -n 2 | gmx rms -s md.tpr -f md.xtc -o rmsd_md.xvg
-echo 0 | gmx rmsf -s md.tpr -f md.xtc -o rmsf_md.xvg
-echo 0 | gmx gyrate -s md.tpr -f md.xtc -o gyrate_md.xvg
-
-#Shoutouts
-echo " "
-echo -e "\033[38;5;208m'The computer was born to solve problems that did not exist before.' — Bill Gates, Microsoft founder and former CEO\033[0m"
-echo " "
-echo -e "\033[38;5;226mYou made it to the end of the script. That can't be right...\033[0m"
-echo -e "\033[38;5;226m:)\033[0m"
-echo " "cho -e "\033[38;5;226mNow running the real deal...\033[0m"
+echo -e "\033[38;5;226mNow running the real deal...\033[0m"
 echo "${run_mdp}" > run.mdp
-gmx grompp -f ~/Project/OMM/run.mdp -c npt.gro -p topol.top -o md.tpr -maxwarn 1
-gmx mdrun -v -deffnm md -s md.tpr -nt $nt
+gmx grompp -f ~/Project/OMM/run.mdp -c npt.gro -p topol.top -o md.tpr -maxwarn 1 || error_exit
+gmx mdrun -v -deffnm md -s md.tpr -nt $nt || error_exit
 
 #Analysis (echoes '0' for whole system)
 echo -e "\033[38;5;226mCalculating RMSD, RMSF, and Rg after run...\033[0m"
-yes 0 | head -n 2 | gmx rms -s md.tpr -f md.xtc -o rmsd_md.xvg
-echo 0 | gmx rmsf -s md.tpr -f md.xtc -o rmsf_md.xvg
-echo 0 | gmx gyrate -s md.tpr -f md.xtc -o gyrate_md.xvg
+yes 0 | head -n 2 | gmx rms -s md.tpr -f md.xtc -o rmsd_md.xvg || error_exit
+echo 0 | gmx rmsf -s md.tpr -f md.xtc -o rmsf_md.xvg || error_exit
+echo 0 | gmx gyrate -s md.tpr -f md.xtc -o gyrate_md.xvg || error_exit
 
 #Shoutouts
 echo " "
